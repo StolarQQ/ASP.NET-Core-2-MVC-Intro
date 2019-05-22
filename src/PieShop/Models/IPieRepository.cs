a1b29a0d006396b1dc6e05cf0c5c2f141b882b66
Stolar <stolar28@gmail.com>
2019-05-22 23:51:20 +0200

    Added pie model

---
+++ src/PieShop/Models/IPieRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace PieShop.Models
+{
+    interface IPieRepository
+    {
+        IEnumerable<Pie> GetAllPies();
+
+        Pie GetPieById(int pieId);
+    }
+}
---
+++ src/PieShop/Models/PieRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PieShop.Models
+{
+    public class PieRepository : IPieRepository
+    {
+        private IEnumerable<Pie> Pies = new List<Pie>
+        {
+            new Pie
+            {
+                Id = 1, ImageThumbnailUrl = "test", ImageUrl = "test",
+                IsInStock = true, IsPieOfTheWeek = true, ShortDescription = "test",
+                LongDescription = "test123", Name = "kozak", Price = 10
+            },
+            new Pie
+            {
+                Id = 2, ImageThumbnailUrl = "test1", ImageUrl = "tes1t",
+                IsInStock = true, IsPieOfTheWeek = true, ShortDescription = "tes1t",
+                LongDescription = "test123", Name = "kozak1", Price = 101
+            }
+        };
+        
+        public IEnumerable<Pie> GetAllPies()
+        {
+            return Pies;
+        }
+
+        public Pie GetPieById(int pieId)
+        {
+            return Pies.SingleOrDefault(x => x.Id == pieId);
+        }
+    }
+}
