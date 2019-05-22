5983301b41908f48d7185f9edc8cc96bd98238d9
Stolar <stolar28@gmail.com>
2019-05-22 23:51:09 +0200

    Setup MVC project

---
+++ src/PieShop/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using PieShop.Models;
+
+namespace PieShop
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddTransient<IPieRepository, PieRepository>();
+        }
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStatusCodePages();
+            app.UseSpaStaticFiles();
+            app.UseMvcWithDefaultRoute();
+
+        }
+    }
+}
