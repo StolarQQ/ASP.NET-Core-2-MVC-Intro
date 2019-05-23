fc6d0765cff0ed4343b7773d6acc24e8513dc4a8
Stolar <stolar28@gmail.com>
2019-05-23 23:59:02 +0200

    Add Inital Controler and Views

---
+++ src/PieShop/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using PieShop.Models;
+using PieShop.ViewModels;
+
+namespace PieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var cakes = _pieRepository.GetAllPies().OrderBy(x => x.Name);
+            
+            var homeViewModel = new HomeViewModel()
+            {
+                Title = "Welcome to Bethany Shop",
+                Pies = cakes.ToList()
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}
